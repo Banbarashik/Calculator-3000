0f9adecd83877984e6e6293c87addda5fc106529
Banbarashik <64987973+Banbarashik@users.noreply.github.com>
2021-12-02 16:35:28 +0700

    Squashed commit of the following:
    commit bfba17d8a81bb6a1fc3e7d3ab47189fe037e678a
    Author: Banbarashik <64987973+Banbarashik@users.noreply.github.com>
    Date:   Thu Dec 2 16:12:41 2021 +0700
    
        Added a possibility to insert negative numbers
    
    commit a8afdee6a1bd6349019076a9640f0e12ae68d3ef
    Author: Banbarashik <64987973+Banbarashik@users.noreply.github.com>
    Date:   Thu Dec 2 04:28:16 2021 +0700
    
        Changed png to svg, reworked layout's css-code
    
    commit 53a25ca8d508db84a9d1538e470cacbc8486ad99
    Author: Banbarashik <odinokyskitalec@yahoo.com>
    Date:   Thu Dec 2 00:03:23 2021 +0700
    
        Restricted evaluation if there's no both operands
    
    commit 81f4b52dd716f9305c5ec703cd1148f459b08dd9
    Author: Banbarashik <odinokyskitalec@yahoo.com>
    Date:   Wed Dec 1 23:52:35 2021 +0700
    
        Added random snarky messages if the user tries to divide by 0

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const buttons = document.querySelectorAll(".number");
 const point = document.querySelector("#point");
 const operators = document.querySelectorAll(".operator");
 const equal = document.querySelector("#equal");
-const clear = document.querySelector("#clear");
+const clearBtn = document.querySelector("#clear");
 const colorSwitchCheckbox = document.querySelector("#color-switch-checkbox");
 const soundSwitchCheckbox = document.querySelector("#sound-switch-checkbox");
 const root = document.documentElement;
@@ -45,12 +45,26 @@ function operate(op, a, b) {
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (display.textContent.length < 20) {
-      if (displayPrevValue && result) {
+      if (
+        displayPrevValue !== null &&
+        displayPrevValue !== "" &&
+        result !== null &&
+        result !== "" &&
+        display.textContent !== "-"
+      ) {
         displayPrevValue = "";
         display.textContent = displayPrevValue;
         indicator = false;
       }
-      display.textContent += e.target.value;
+      if (
+        currentOperator === "-" &&
+        display.textContent.length < 1 &&
+        !prevValue
+      ) {
+        display.textContent = currentOperator + e.target.value;
+      } else {
+        display.textContent += e.target.value;
+      }
 
       if (e.target.value === ".") {
         point.setAttribute("disabled", "");
@@ -61,13 +75,34 @@ buttons.forEach((button) => {
 
 operators.forEach((operator) => {
   operator.addEventListener("click", (e) => {
-    if (currentOperator === e.target.value && !indicator && result) {
+    if (e.target.value === "-" && display.textContent.length < 1) {
+      display.textContent = e.target.value;
+    }
+    if (
+      currentOperator === e.target.value &&
+      !indicator &&
+      result &&
+      prevValue &&
+      isNaN(prevValue) === false &&
+      display.textContent
+    ) {
       result = operate(currentOperator, +prevValue, +display.textContent);
       indicator = true;
       display.textContent = parseFloat(result.toFixed(10));
       displayPrevValue = true;
       currentOperator = "";
       point.removeAttribute("disabled", "");
+
+      if (isNaN(result)) {
+        clear();
+      }
+
+      if (result === Infinity || result === -Infinity) {
+        display.textContent =
+          Math.random() > 0.5
+            ? "The wasteland has claimed your life."
+            : "The Matrix has you...";
+      }
     } else {
       currentOperator = e.target.value;
       prevValue = display.textContent;
@@ -97,8 +132,15 @@ equal.addEventListener("click", () => {
     displayPrevValue = true;
     point.removeAttribute("disabled", "");
 
-    if (result === Infinity) {
-      display.textContent = "The wasteland has claimed your life.";
+    if (isNaN(result)) {
+      clear();
+    }
+
+    if (result === Infinity || result === -Infinity) {
+      display.textContent =
+        Math.random() > 0.5
+          ? "The wasteland has claimed your life."
+          : "The Matrix has you...";
     }
 
     document
@@ -107,7 +149,10 @@ equal.addEventListener("click", () => {
   }
 });
 
-clear.addEventListener("click", () => {
+clearBtn.addEventListener("click", clear);
+
+function clear() {
+  currentOperator = "";
   prevValue = "";
   displayPrevValue = "";
   display.textContent = "";
@@ -118,24 +163,28 @@ clear.addEventListener("click", () => {
   document
     .querySelector(".current-operator")
     .classList.remove("current-operator");
-});
+}
 
 const highlightSound = document.querySelector("#highlight");
 const selectSound = document.querySelector("#select");
+const lightOnSound = document.querySelector("#light");
 function playSound(sound) {
   sound.currentTime = 0;
   sound.play();
 }
 highlightSound.muted = true;
 selectSound.muted = true;
+lightOnSound.muted = true;
 
 soundSwitchCheckbox.addEventListener("click", () => {
   if (soundSwitchCheckbox.checked === true) {
     highlightSound.muted = false;
     selectSound.muted = false;
+    lightOnSound.muted = false;
   } else {
     highlightSound.muted = true;
     selectSound.muted = true;
+    lightOnSound.muted = true;
   }
 });
 
@@ -145,13 +194,27 @@ allButtons.forEach((button) => {
 });
 
 colorSwitchCheckbox.addEventListener("click", () => {
+  playSound(lightOnSound);
+});
+
+colorSwitchCheckbox.addEventListener("click", () => {
   if (colorSwitchCheckbox.checked === true) {
     root.style.setProperty("--bg-color", "rgb(74 63 37)");
     root.style.setProperty("--main-color", "rgb(42, 33, 13)");
     root.style.setProperty("--font-color", "rgb(219, 174, 62)");
+    changeColor("rgb(219, 174, 62)");
   } else {
     root.style.setProperty("--bg-color", "rgb(61, 85, 65)");
     root.style.setProperty("--main-color", "rgb(3, 27, 14)");
     root.style.setProperty("--font-color", "rgb(32, 192, 104)");
+    changeColor("rgb(32, 192, 104)");
   }
 });
+
+function changeColor(color) {
+  const svg = document.querySelector("#vault-boy").contentDocument;
+  const svgElements = svg.querySelectorAll(".st0");
+  svgElements.forEach((elem) => {
+    elem.style.fill = color;
+  });
+}
